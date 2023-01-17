3f51fb5e336d53f1d91964ab3ca4ea807b6f164e
Sarah.Berbuir <livine.gmks@gmail.com>
2023-01-17 21:14:01 +0100

    File to handle programming of servomotors

---
+++ Rasperry_Pi_Pico/position_servos.py
@@ -0,0 +1,9 @@
+def start_execution(selectedPositions):
+
+    #for pos in selectedPositions:
+    position = [4400, 3600, 4000, 5000, 3000]
+    #if selectedPositions[0][1] < 4:
+     #   position = [4400, 3600, 4000, 5000, 3000]
+    #else:
+     #   position = [4400, 3300, 5000, 5000, 1000]
+    return position
