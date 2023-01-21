03d5b55b95bb1e6aa96f85215f29fa3cd6aca3bb
Sarah.Berbuir <livine.gmks@gmail.com>
2023-01-21 12:53:11 +0100

    Refactor and transform code structure

--- Rasperry_Pi_Pico/position_servos.py
+++ Rasperry_Pi_Pico/position_servos.py
@@ -1,9 +1,68 @@
-def start_execution(selectedPositions):
 
-    #for pos in selectedPositions:
-    position = [4400, 3600, 4000, 5000, 3000]
-    #if selectedPositions[0][1] < 4:
-     #   position = [4400, 3600, 4000, 5000, 3000]
-    #else:
-     #   position = [4400, 3300, 5000, 5000, 1000]
-    return position
+# After clicking on Start this function is executed. 
+# It starts animation of robot arm and changes from start_pos to pos
+
+from machine import Pin, PWM
+import machine
+from time import sleep
+
+#Initializing Pins of Raspberry Pi Pico W
+pwm0 = PWM(Pin(0))
+pwm2 = PWM(Pin(1))
+pwm1 = PWM(Pin(2))
+pwm3 = PWM(Pin(3))
+pwm4 = PWM(Pin(4))
+
+# Frequence of 50 for servomotors
+pwm0.freq(50)
+pwm1.freq(50)
+pwm2.freq(50)
+pwm3.freq(50)
+pwm4.freq(50)
+
+# Save positions for transformation of servomotors
+startPosition0 = [5500,3600,4000,5000,6500]
+position0 = [4400, 3600, 4000, 5000, 3500]
+startPosition1 = [4400, 3600, 4000, 5000, 3500]
+position1 = [4400, 3800, 5000, 5000, 1000]
+
+def startAnalysis(start_pos, pos):
+    pwm0.duty_u16(start_pos[0])
+    pwm1.duty_u16(start_pos[1])
+    pwm2.duty_u16(start_pos[2])
+    pwm3.duty_u16(start_pos[3])
+    pwm4.duty_u16(start_pos[4])
+    sleep(2)
+    pwm0.duty_u16(pos[0])
+    pwm1.duty_u16(pos[1])
+    pwm2.duty_u16(pos[2])
+    pwm3.duty_u16(pos[3])
+    pwm4.duty_u16(pos[4])    
+
+# Transform request and handle interactions of start and reset button    
+def transform_request(request, selected_positions):
+    try:
+        request = request.split()[1]
+        if  request[1] == "(":
+            if len(selected_positions)==2:
+                selected_positions.clear() 
+            row = request[2]
+            col = request[4]
+            request = request[2:-2]
+            item = [int(row), int(col)]
+            if not item in selected_positions:
+                selected_positions.append(item)
+        print(selected_positions)
+    except IndexError:
+        pass
+    print(request)
+    if request == '/startAnalysis?':
+        print("start")
+        if selected_positions[0][1] >= 4:
+            startAnalysis(startPosition0, position0) 
+        else:
+            startAnalysis(startPosition1, position1) 
+    elif request == '/resetSelection?':
+        print("reset")
+        selected_positions.clear()   
+    return selected_positions
--- Rasperry_Pi_Pico/web_server.py
+++ Rasperry_Pi_Pico/web_server.py
@@ -1,40 +1,24 @@
+# Web_server includes all functions to set up the connection to the wifi, 
+# to build the HTML code in the website and to control the servo motors.
+# Run this file to perform Chembot's main function.
+
 from time import sleep
 from machine import Pin, PWM
 import socket
-from picozero import pico_temp_sensor, pico_led
 import machine
 import network
 import html_code
 import position_servos
 import wifi_credentials
 
-pwm0 = PWM(Pin(0))
-pwm2 = PWM(Pin(1))
-pwm1 = PWM(Pin(2))
-pwm3 = PWM(Pin(3))
-pwm4 = PWM(Pin(4))
-
-pwm0.freq(50)
-pwm1.freq(50)
-pwm2.freq(50)
-pwm3.freq(50)
-pwm4.freq(50)
-
-already_started = False
+# Save positions user clicked
 selected_positions = []
 
-startPosition0 = [5500,3600,4000,5000,6500]
-position0 = [4400, 3600, 4000, 5000, 3500]
-startPosition1 = [4400, 3600, 4000, 5000, 3500]
-position1 = [4400, 3800, 5000, 5000, 1000]
-
-
+# Get wifi credentials # TODO fill out file
 ssid, password = wifi_credentials.get_credentials()
 
-
-
+#Connect to WLAN
 def connect():
-    #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
@@ -43,76 +27,32 @@ def connect():
         sleep(1)
     print(wlan.ifconfig())
     ip = wlan.ifconfig()[0]
+    # Show ip address to find website of Chembot
     print(f'Connected on {ip}')
-    
     return ip
 
+# Open a socket for website
 def open_socket(ip):
-    # Open a socket
     address = (ip, 80)
     connection = socket.socket()
     connection.bind(address)
     connection.listen(1)
     return connection
 
-def startAnalysis1(start_pos, pos):
-    pwm0.duty_u16(start_pos[0])
-    pwm1.duty_u16(start_pos[1])
-    pwm2.duty_u16(start_pos[2])
-    pwm3.duty_u16(start_pos[3])
-    pwm4.duty_u16(start_pos[4])
-    sleep(2)
-    pwm0.duty_u16(pos[0])
-    pwm1.duty_u16(pos[1])
-    pwm2.duty_u16(pos[2])
-    pwm3.duty_u16(pos[3])
-    pwm4.duty_u16(pos[4])    
-    
-        
+# Start a web server and handle interactions with website
 def serve(connection):
-    #Start a web server
-    state = 'OFF'
-    pico_led.off()
-    temperature = 0
     while True:
         client = connection.accept()[0]
         request = client.recv(1024)
         request = str(request)
         print(request)
-        try:
-            request = request.split()[1]
-            if  request[1] == "(":
-                if len(selected_positions)==2:
-                    selected_positions.clear() 
-                row = request[2]
-                col = request[4]
-                request = request[2:-2]
-                item = [int(row), int(col)]
-                if not item in selected_positions:
-                    selected_positions.append(item)
-            print(selected_positions)
-        except IndexError:
-            pass
-        print(request)
-        if request == '/startAnalysis?':
-        
-
-            if selected_positions[0][1] >= 4:
-                startAnalysis1(startPosition0, position0) 
-            else:
-                startAnalysis1(startPosition1, position1) 
-        elif request == '/resetSelection?':
-            print("reset")
-            selected_positions.clear() 
-
-        state = selected_positions
-        temperature = pico_temp_sensor.temp
-        html = html_code.getHTML(state)
+        selected_positions = position_servos.transform_request(request, selected_positions)
+        html = html_code.getHTML(selected_positions)
         client.send(html)
         client.close()
-    
-try:
 
+# Start complete webserver
+try:
     ip = connect()
     connection = open_socket(ip)
     serve(connection)
